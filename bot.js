f7adc8ad8681f6fd04e1a5b7d1ecdd3ba97f7bba
Josh Cash <master_cash@live.com>
2018-04-22 16:30:31 -0500

    added comments

--- bot.js
+++ bot.js
@@ -2,76 +2,117 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 let fs = require('fs');
 
+//ID of the bot from discord
 var botID = "8691b3fc456f2eb6539908d798";
 
+//function to aquire the the response of the bot to any post.
 function respond() 
 {
-  console.log("BOI GOT THAT LOG!");
+  //A post from the groupme, that is parsed from json to an obj
   var request = JSON.parse(this.req.chunks[0]),
+
+  //the array of regex commands to be checked for.
   botRegex = new Array(/^\/cool guy$/, /^\/talk$/);
   
-
-  if(request.text && botRegex[0].test(request.text)) {
+  //check for a message then check to see if it matches the first regex
+  if(request.text && botRegex[0].test(request.text)) 
+  {
+    //writes a http status code of 200, which means the request has succeeded. 
     this.res.writeHead(200);
+    //calls the function to get the message
     postMessage(0, request);
+    //finishes sending the request
     this.res.end();
   }
-  else if(request.text && botRegex[1].test(request.text)) {
-    postMessage(1, request);
+  //check for a message then check to se if it matches the second regex
+  else if(request.text && botRegex[1].test(request.text))
+  {
+    //writes a http status code of 200, which means the request has succeeded.
     this.res.writeHead(200);
+    //calls the function to get the message
+    postMessage(1, request);
+    //finishes sending the request 
     this.res.end();
   }
 }
 
-function postMessage(x, request) {
+//sets the appropriate message for the response given.
+function postMessage(x, request) 
+{
+  // variables for holding information.
   var botResponse, options, body, botReq;
-  
+  //if it was the first regex get that cool face.
   if(x === 0)
-  botResponse = cool();
+    botResponse = cool();
+  //then must be the second regex response.
   else 
   {
+    //set the response to mentioning who made command and the following message.
     botResponse = "@" + request.name + " Talk dirty to Me! ";
+    // the structure for mentioning people.
     var attachments = {
       loci: [ [botResponse.indexOf("@" + request.name), request.name.length + 1] ],
       type: "mentions",
       user_ids: [request.user_id]
       };
   }
+
+  //information of where to send it to and type.
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST'
   };
   
-  
-  
-  body = {
-    "attachments" : [attachments],
-    "bot_id" : botID,
-    "text" : botResponse
-  };
+  // if it was the second type then add the mentioning.
+  if(x === 1)
+  {
+    
+    body = {
+      "attachments" : [attachments],
+      "bot_id" : botID,
+      "text" : botResponse
+    };
+  }
 
-  let mineJSON = JSON.stringify(body);
-  fs.writeFile("mine.json", mineJSON, function(err) {if(err) console.log(err);});
+  // else set up without mentioning.
+  else
+  {
+    body = {
+      "bot_id" : botID,
+      "text" : botResponse
+    }
+  }
 
+  //DEBUG what message was sent.
   console.log('sending ' + botResponse + ' to ' + botID);
   
-  botReq = HTTPS.request(options, function(res) {
-    if(res.statusCode == 202) {
-      //neat
-    } else {
-      console.log('rejecting bad status code ' + res.statusCode);
-    }
-  });
+  //sent the request at the location set in options.
+  botReq = 
+    HTTPS.request(options, function(res) 
+    {
+      //check to see if the request was accepted.
+      if(res.statusCode == 202) {/* went through*/ } 
+      else 
+      {
+        //was rejected with the following code.
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+    });
   
-  botReq.on('error', function(err) {
+  //error handling for the request.
+  botReq.on('error', function(err) 
+  {
     console.log('error posting message '  + JSON.stringify(err));
   });
-  botReq.on('timeout', function(err) {
+  // error handling for timeout on request.
+  botReq.on('timeout', function(err) 
+  {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
+  //close connection with message.
   botReq.end(JSON.stringify(body));
 }
 
-
+//No Idea what this is for.
 exports.respond = respond;
--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 var http, director, cool, bot, router, server, port;
 
+//required dependencies
 http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
+//Log attempting to start the Bot.
 console.log("BOi we starting up!");
 
-router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
-});
+//creating a new router that manages incoming and outgoing messages.
+//Adds the corresponding fields of post and get of http
+router = 
+  new director.http.Router(
+  {
+    //setting the message the root.
+    '/' : 
+    {
+      post: bot.respond,
+      get: ping
+    }
+  });
 
-
+// creation of the server 
 server = http.createServer(function (req, res) 
 {
+  //array of the infomation sent ? not sure.
   req.chunks = [];
+  // parsing of the information.
   req.on('data', function (chunk) { req.chunks.push(chunk.toString()); });
-
+  //not sure what this is exactly catch errors possibly?
   router.dispatch(req, res, function(err) 
   {
+    //writing the error message out ??
     res.writeHead(err.status, {"Content-Type": "text/plain"});
+    //ending the write?
     res.end(err.message);
   });
 });
+
+//making sure the server actually started.
 if(server !== null)
   console.log("We starting up that server!");
 
-port = Number(process.env.PORT || 80);
+//setting the port for listening
+port = Number(80);
+//start listening on that port.
 server.listen(port);
 
+//log the address information of server.
 console.log(server.address());
 
+//this is for get of http should only be on website access.
 function ping() {
   this.res.writeHead(200);
   this.res.end("Hey, I'm Cool Guy.");
