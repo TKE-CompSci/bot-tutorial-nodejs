a1d1e696e1b29c592185aaf1e4ca494149e1eafe
MasterCash <MasterCash@users.noreply.github.com>
2018-04-22 07:13:42 +0000

    isolation of ids

--- bot.js
+++ bot.js
@@ -8,9 +8,16 @@ function respond()
   console.log("BOI GOT THAT LOG!");
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = new Array(/^\/cool guy$/, /^\/talk$/);
-  
-  console.log(request.name);
+  var options = {
+  loci: "",
+  type: "",
+  user_id: ""
+};
 
+  console.log(request);
+  if(request.attachments[0].user_ids !== undefined) 
+    console.log(request.attachments[0].user_ids);
+  else console.log("Does not exist!");
   if(request.text && botRegex[0].test(request.text)) {
     this.res.writeHead(200);
     postMessage(0, request);
@@ -29,7 +36,7 @@ function postMessage(x, request) {
   if(x === 0)
     botResponse = cool();
   else 
-    botResponse = "Talk dirty to Me! @"+ request.name;
+    botResponse = "Talk dirty to Me! @"+ request.user_id+" @"+request.sender_id;
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
