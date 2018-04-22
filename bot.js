aef8604448b3604ec4730d9e0dd4da9aabc9f050
Josh Cash <master_cash@live.com>
2018-04-22 02:41:23 -0500

    change command

--- bot.js
+++ bot.js
@@ -7,13 +7,8 @@ function respond()
 {
   console.log("BOI GOT THAT LOG!");
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = new Array(/^\/cool guy$/, /^\/talk$/);
-  var options = {
-  loci: [1, 8],
-  type: 'mentions',
-  user_id: ['35587100']
-};
-
+  botRegex = new Array(/^\/cool guy$/, /^\/talk$/);
+  
   console.log(request);
   
   if(request.text && botRegex[0].test(request.text)) {
@@ -30,33 +25,39 @@ function respond()
 
 function postMessage(x, request) {
   var botResponse, options, body, botReq;
-
+  
   if(x === 0)
-    botResponse = cool();
+  botResponse = cool();
   else 
-    botResponse = "@Josh Cash Talk dirty to Me! ";
+  botResponse = "@Josh Cash Talk dirty to Me! ";
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST'
   };
-
+  
+  var op = {
+  loci: [1, request.name.length],
+  type: 'mentions',
+  user_id: [request.user_id]
+  };
+  
   body = {
-    "attachments" : options,
+    "attachments" : [op],
     "bot_id" : botID,
     "text" : botResponse
   };
-
+  
   console.log('sending ' + botResponse + ' to ' + botID);
-
+  
   botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
+    if(res.statusCode == 202) {
+      //neat
+    } else {
+      console.log('rejecting bad status code ' + res.statusCode);
+    }
   });
-
+  
   botReq.on('error', function(err) {
     console.log('error posting message '  + JSON.stringify(err));
   });
