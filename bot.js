67a25c37c5082413a41261f26c78bcc3cb190d67
Josh Cash <master_cash@live.com>
2018-05-14 22:03:15 -0500

    adding a delay to messages

--- bot.js
+++ bot.js
@@ -377,7 +377,7 @@ function message(x, isInternal, request) {
  * @param {request} request - information on user who sent message that regex matched.
  */
 function postMessage(x, isInternal, request) {
-    setTimeout(message, 250, x, isInternal, request);
+    setTimeout(message, 260, x, isInternal, request);
 }
 
 /**
